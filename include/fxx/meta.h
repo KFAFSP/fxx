8c2b21b9ef4a4d3172644f4403d6c2c72859d007
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-06-04 23:04:03 +0200

    Added tuple metaprogramming.

---
+++ include/fxx/meta.h
@@ -0,0 +1,28 @@
+/** Metaprogramming namespace.
+ *
+ * @file        meta.h
+ *
+ * @author      Karl F. A. Friebel (karl.friebel@friebelnet.de)
+ *
+ * @version     0.2
+ * @date        2019-06-04
+ *
+ * @copyright   Copyright (c) 2019
+ */
+
+#ifndef FXX_META_H
+#define FXX_META_H
+#pragma once
+
+#include <fxx/meta/tuple.h>
+
+namespace fxx {
+
+/** Metaprogramming namespace. */
+namespace meta {
+
+} // namespace meta
+
+} // namespace fxx
+
+#endif
---
+++ test/src/meta.cpp
@@ -0,0 +1,4 @@
+// Enables static testing
+#define FXX_TEST_STATIC
+
+#include <fxx/meta/tuple.h>
